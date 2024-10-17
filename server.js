7bb233d6bfafe5974daec5027d56481ebd5b0a44
MiniCubo <minicubo121@gmail.com>
2024-10-17 10:45:08 -0600

    Orden

--- server.js
+++ server.js
@@ -11,58 +11,60 @@ app.set("view engine", "ejs");
 
 var info = []
 
-
 var initialized = false;
 app.use("/", (req, res, next)=>{
     if(!initialized){
         const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
-        https.get(url, (response) => {
-            var data = "";
-            response.on("data", (chunk) => {
-                data += chunk;
-            }).on("end", async () => {
-                var pokedex = JSON.parse(data);
-                var pokemons = pokedex.results;
+    https.get(url, (response) => {
+        var data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        }).on("end", async () => {
+            var pokedex = JSON.parse(data);
+            var pokemons = pokedex.results;
 
-                var pokemonPromises = pokemons.map(async (pokemon) => {
-                    return new Promise(async(resolve, reject) => {
-                        try {
-                            const urlPokemon = pokemon.url;
-                            var data2 = "";
-                            var b = https.get(urlPokemon, (response2) => {
-                                response2.on("data", (chunk2) => {
-                                    data2 += chunk2;
-                                }).on("end", () => {
-                                    var poke = JSON.parse(data2);
-                                    var name = poke.name;
-                                    var sprite = poke.sprites.front_default;
-                                    const pokemonInfo = {
-                                        name: name,
-                                        sprite: sprite
-                                    };
-                                    info.push(pokemonInfo);
-                                    resolve(); 
-                                });
-                            }).on("error", (error) => reject(error)); 
-                        } catch (error) {
-                            reject(error);
-                        }
-                    });
-                });
-                await Promise.all(pokemonPromises);
-                initialized = true;
-                next();
-            });
-        }).on("error", (error) => {
-            console.error(`error: ${error}`);
+            for (let pokemon of pokemons) {
+                try {
+                    const urlPokemon = pokemon.url;
+                    var data2 = await fetchPokemonData(urlPokemon);
+                    var poke = JSON.parse(data2);
+                    var name = poke.name;
+                    var sprite = poke.sprites.front_default;
+                    const pokemonInfo = {
+                        name: name,
+                        sprite: sprite
+                    };
+                    info.push(pokemonInfo); 
+                } catch (error) {
+                    console.error(`error: ${error}`);
+                }
+            }
+            initialized = true;
+            next();
         });
+    }).on("error", (error) => {
+        console.error(`error: ${error}`);
+    });
     }
     else{
         next();
     }
 });
 
-
+function fetchPokemonData(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+            var data = "";
+            response.on("data", (chunk) => {
+                data += chunk;
+            }).on("end", () => {
+                resolve(data);  
+            }).on("error", (error) => {
+                reject(error);  
+            });
+        });
+    });
+}
 
 app.route("/")
 .get((req, res)=>{
