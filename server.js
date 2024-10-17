bdbdedb92922661a87521ad30b29aed805e4989d
MiniCubo <minicubo121@gmail.com>
2024-10-17 13:49:22 -0600

    Finished the BackEnd

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
 const https = require("https");
@@ -10,11 +11,13 @@ app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 var info = []
+var index = 0;
+const tamPerPage = 20;
 
-var initialized = false;
-app.use("/", (req, res, next)=>{
-    if(!initialized){
-        const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
+app.use("/pokedex/:index", (req, res, next)=>{
+    index = req.params.index;
+    info = [];
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${tamPerPage}&offset=${index*tamPerPage}`;
     https.get(url, (response) => {
         var data = "";
         response.on("data", (chunk) => {
@@ -46,10 +49,7 @@ app.use("/", (req, res, next)=>{
         console.error(`error: ${error}`);
     });
     }
-    else{
-        next();
-    }
-});
+);
 
 function fetchPokemonData(url) {
     return new Promise((resolve, reject) => {
@@ -66,16 +66,75 @@ function fetchPokemonData(url) {
     });
 }
 
+function buildPokemonData(data) {
+    return new Promise((resolve) => {
+        var poke = JSON.parse(data);
+        var name = poke.name;
+        var id = poke.id;
+        var height = poke.height/10;
+        var experience = poke.base_experience;
+        var sprite = poke.sprites.front_default;
+        var type = poke.types;
+        var weight = poke.weight;
+        var stats = poke.stats;
+        const pokemonInfo = {
+            name: name,
+            sprite: sprite,
+            id: id,
+            h: height,
+            xp: experience,
+            types: type,
+            w: weight,
+            stats: stats
+        };
+        resolve(pokemonInfo);
+        });
+}
+
 app.route("/")
 .get((req, res)=>{
-    res.send(info)
+    res.redirect("/pokedex/0");
 });
 
-app.post("/search", (req, res)=>{
-    var pokemonName = req.body.name;
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-    
+app.get("/pokedex/:index", (req,res)=>{
+    res.render("index2", {info, tamPerPage});
+    //res.render("index", {});
 });
+
+var pokemon = "";
+
+app.post("/search", async (req, res)=>{
+    var pokemonName = req.body.name;
+    var pokemonID = req.body.id;
+    var placeholder = "";
+    if (pokemonName) placeholder = pokemonName
+    else placeholder = pokemonID
+    const url = `https://pokeapi.co/api/v2/pokemon/${placeholder}`;
+    var data = await fetchPokemonData(url);
+    if(data == "Not Found"){
+        res.redirect("/error");
+    }
+    else{
+        pokemon = await buildPokemonData(data);
+        res.redirect("/pokemon");
+    }
+});
+
+app.get("/pn/:id", async (req, res)=>{
+    var pokemonID = req.params.id;
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`;
+    var data = await fetchPokemonData(url);
+    pokemon = await buildPokemonData(data);
+    res.redirect("/pokemon");
+});
+
+app.get("/pokemon", (req, res)=>{
+    res.render("pokemon", {pokemon:pokemon});
+});
+
+app.get("/error", (req, res)=>{
+    res.render("error", {});
+})
 
 app.listen(3000, ()=>{
     console.log("Listening to port 3000");
