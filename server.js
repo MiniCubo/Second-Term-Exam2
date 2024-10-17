72f704f5be8bd1c9d856953f531012c2e5c6a74d
MiniCubo <minicubo121@gmail.com>
2024-10-17 10:32:28 -0600

    2

--- server.js
+++ server.js
@@ -9,35 +9,64 @@ app.use(express.static(__dirname+ "/public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
+var info = []
+
+
+var initialized = false;
+app.use("/", (req, res, next)=>{
+    if(!initialized){
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
+        https.get(url, (response) => {
+            var data = "";
+            response.on("data", (chunk) => {
+                data += chunk;
+            }).on("end", async () => {
+                var pokedex = JSON.parse(data);
+                var pokemons = pokedex.results;
+
+                var pokemonPromises = pokemons.map(async (pokemon) => {
+                    return new Promise(async(resolve, reject) => {
+                        try {
+                            const urlPokemon = pokemon.url;
+                            var data2 = "";
+                            var b = https.get(urlPokemon, (response2) => {
+                                response2.on("data", (chunk2) => {
+                                    data2 += chunk2;
+                                }).on("end", () => {
+                                    var poke = JSON.parse(data2);
+                                    var name = poke.name;
+                                    var sprite = poke.sprites.front_default;
+                                    const pokemonInfo = {
+                                        name: name,
+                                        sprite: sprite
+                                    };
+                                    info.push(pokemonInfo);
+                                    resolve(); 
+                                });
+                            }).on("error", (error) => reject(error)); 
+                        } catch (error) {
+                            reject(error);
+                        }
+                    });
+                });
+                await Promise.all(pokemonPromises);
+                initialized = true;
+                next();
+            });
+        }).on("error", (error) => {
+            console.error(`error: ${error}`);
+        });
+    }
+    else{
+        next();
+    }
+});
+
+
+
 app.route("/")
 .get((req, res)=>{
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
-    https.get(url, (response)=>{
-        var data = "";
-        response.on("data", (chunk)=>{
-            data += chunk;
-    }).on("end", ()=>{
-        var pokedex = JSON.parse(data);
-        var pokemons = pokedex.results;
-        info = []
-        pokemons.forEach((pokemon)=>{
-            try{
-                const urlPokemon = pokemon.url;
-                var data2 = "";
-                https.get(urlPokemon, (response2)=>{
-                    response2.on("data", (chunk2)=>{
-                        data2 += chunk2;
-                    }).on("end", ()=>{
-                        var poke = JSON.parse(data2);
-                        
-                    })
-                })
-            }catch(error){
-    
-            }
-        })
-        });
-    });
+    res.send(info)
 });
 
 app.post("/search", (req, res)=>{
