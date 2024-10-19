0b2cc101e7f59138536dadaaec12fd7c74798c46
MiniCubo <minicubo121@gmail.com>
2024-10-18 22:34:05 -0600

    A

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get('*', (req,res) => 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 app.engine("ejs", require("ejs").renderFile);
