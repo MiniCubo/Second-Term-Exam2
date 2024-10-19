9ab861b458f8ca924bf7707149bf7967f4bf54d2
MiniCubo <minicubo121@gmail.com>
2024-10-18 22:36:34 -0600

    B

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ require("dotenv").config({});
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-//app.use(express.static(__dirname+ "/public"));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => 
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+app.use(express.static(__dirname+ "/public"));
 
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
